8080dd54d7a839f6137b81ab135e7b40f4259adc
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2020-03-04 17:00:15 +0100

    updat

--- database/seeds/LgaSeeder.php
+++ database/seeds/LgaSeeder.php
@@ -2,13 +2,25 @@
 
 use Illuminate\Database\Seeder;
 use App\Model\Lga;
+use Flynsarmy\CsvSeeder\CsvSeeder;
 
-class LgaSeeder extends Seeder
+class LgaSeeder extends CsvSeeder
 {
+	public function __construct()
+	{
+		$this->table = 'lgas';
+		$this->filename = base_path().'/database/seeds/Excel/new-lgas.xlsx';
+	}
+
     public function run()
     {
     	DB::statement("SET FOREIGN_KEY_CHECKS=0");
         Lga::truncate();
         DB::statement("SET FOREIGN_KEY_CHECKS=1");
+
+        // Recommended when importing larger CSVs
+		// DB::disableQueryLog();
+
+		parent::run();
     }
 }
--- database/seeds/StateSeeder.php
+++ database/seeds/StateSeeder.php
@@ -2,15 +2,25 @@
 
 use Illuminate\Database\Seeder;
 use App\Model\State;
+use Flynsarmy\CsvSeeder\CsvSeeder;
 
-class StateSeeder extends Seeder
+class StateSeeder extends CsvSeeder
 {
+	public function __construct()
+	{
+		$this->table = 'states';
+		$this->filename = base_path().'/database/seeds/Excel/new-states1.csv';
+	}
+
     public function run()
     {
     	DB::statement("SET FOREIGN_KEY_CHECKS=0");
         State::truncate();
         DB::statement("SET FOREIGN_KEY_CHECKS=1");
 
-        
+        // Recommended when importing larger CSVs
+		// DB::disableQueryLog();
+
+		parent::run();
     }
 }
