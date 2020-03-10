67cae7daf0b605aa740191f4b6e4c7a3afb04183
CNwadike <chimanwadike94@gmail.com>
2020-03-10 23:58:42 +0100

    minor

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,11 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-    	$this->call(RoleSeeder::class);
-        $this->call(StateSeeder::class);
-        $this->call(LgaSeeder::class);
-        $this->call(FacilitySeeder::class);
-        $this->call(UserSeeder::class);
+//    	$this->call(RoleSeeder::class);
+//        $this->call(StateSeeder::class);
+//        $this->call(LgaSeeder::class);
+//        $this->call(FacilitySeeder::class);
+//        $this->call(UserSeeder::class);
         $this->call(ClientSeeder::class);
     }
 }
